a3d0269411d6edaa735aafdbd489f189e0309844
Andrew Clinton <ddrew.clinton@gmail.com>
2016-05-18 16:43:29 -0400

    Added php doc and made note of methods which aren't currently compatible with v4

--- src/QueryParams/v4/OdataQueryParams.php
+++ src/QueryParams/v4/OdataQueryParams.php
@@ -15,31 +15,69 @@ class OdataQueryParams implements OdataQueryParamsInterface
 		$this->request = $request;
 	}
 
+	/**
+	 * @return array|string
+	 */
 	public function getOrderBy()
 	{
 		return $this->request->input('$orderby');
 	}
 
+	/**
+	 * @return array|string
+	 */
 	public function getSkip()
 	{
 		return $this->request->input('$skip');
 	}
 
+	/**
+	 * @return array|string
+	 */
 	public function getTop()
 	{
 		return $this->request->input('$top');
 	}
 
+	/**
+	 * This is currently not fully supported for ODATA v4.
+	 *
+	 * TODO: Fix this method to work with v4 syntax. The below examples do not parse correctly.
+	 *
+	 * Example: $select=DemoService.*
+	 *
+	 * @return array|string
+	 */
 	public function getSelect()
 	{
 		return $this->request->input('$select');
 	}
 
+	/**
+	 * This is currently not fully supported for ODATA v4.
+	 *
+	 * TODO: Fix this method to work with v4 syntax. The below examples do not parse correctly.
+	 *
+	 * Example: $expand=Orders($filter=Amount gt 100)
+	 * Example: $expand=SampleModel.VipCustomer/InHouseStaff
+	 *
+	 * @return array|string
+	 */
 	public function getExpand()
 	{
 		return $this->request->input('$expand');
 	}
 
+	/**
+	 * This is currently not fully supported for ODATA v4.
+	 *
+	 * TODO: Fix this method to work with v4 syntax. The below examples do not parse correctly.
+	 *
+	 * Example: $filter=Style has Sales.Color'Yellow'
+	 * Example: $filter=(Price sub 5) gt 10
+	 *
+	 * @return array|string
+	 */
 	public function getFilter()
 	{
 		return $this->request->input('$filter');
